3580777c1f777510a735c63561b8b4a10ab24d36
giggagit <gigg@outlook.com>
2020-06-07 18:18:48 +0700

    Add gateway-server

---
+++ gateway-server/src/main/java/com/giggagit/gateway/GatewayServerApp.java
@@ -0,0 +1,15 @@
+package com.giggagit.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient 
+@SpringBootApplication
+public class GatewayServerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServerApp.class, args);
+	}
+
+}
---
+++ gateway-server/src/test/java/com/giggagit/gateway/GatewayServerAppTests.java
@@ -0,0 +1,13 @@
+package com.giggagit.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
